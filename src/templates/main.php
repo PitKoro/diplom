60e3cc409777a96a6ee2bc4db12dee1309ddaa00
PitKoro <pit.koro@gmail.com>
2021-04-29 16:27:32 +0300

    Добавил библиотеку apexcharts js

--- src/templates/main.php
+++ src/templates/main.php
@@ -27,11 +27,40 @@ if (!$_SESSION['user']) {
 
     <div class="container mt-5">
         <h1>Это главная страница</h1>
+        <div class="row">
+            <div class="col" id="chart"></div>
+        </div>
+       
     </div>
 
     <!-- Bootstrap and jQuery JS -->
     <script src="../../public/vendor/jquery/jquery-3.5.1.min.js"></script>
     <script src="../../public/vendor/popper/popper.min.js"></script>
     <script src="../../public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../../public/vendor/apexcharts/apexcharts.js"></script>
+    <script>
+
+        var options = {
+            series: [44, 55, 41, 17, 15],
+            chart: {
+            type: 'donut',
+            },
+            responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                width: 350
+                },
+                legend: {
+                position: 'right'
+                }
+            }
+            }]
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
+
+    </script>
 </body>
 </html>
